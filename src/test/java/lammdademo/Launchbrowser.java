27d01795f71989fdd137d04c2ffa8c188ff92b74
vijaylahotis <ecevijay2008@gmail.com>
2023-08-01 19:33:31 +0530

    newlatest

--- src/test/java/lammdademo/Launchbrowser.java
+++ src/test/java/lammdademo/Launchbrowser.java
@@ -11,8 +11,8 @@ public class Launchbrowser {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.lambdatest.com/");
-		String titles = driver.getTitle();
-		System.out.println(titles);
+		String titlesname = driver.getTitle();
+		System.out.println(titlesname);
 		driver.quit();
 	}
 }
