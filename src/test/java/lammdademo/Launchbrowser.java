7030c982fa94117e262125b1af1f1c82c8a9ae82
vijaylahotis <ecevijay2008@gmail.com>
2023-08-01 19:27:05 +0530

    latest

---
+++ src/test/java/lammdademo/Launchbrowser.java
@@ -0,0 +1,18 @@
+package lammdademo;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launchbrowser {
+
+	@Test
+	public void mytest() {
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.lambdatest.com/");
+		String titles = driver.getTitle();
+		System.out.println(titles);
+		driver.quit();
+	}
+}
